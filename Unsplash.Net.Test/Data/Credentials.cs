ead3937cb8790e7ec6527447e90d10ea9bb8de6d
LT <longt@aliyun.com>
2023-06-06 09:56:52 +0800

    update .net

---
+++ Unsplash.Net.Test/Data/Credentials.cs
@@ -0,0 +1,15 @@
+﻿namespace UnsplashsharpTest.Data {
+    public static class Credentials {
+        public static string ApplicationId {
+            get {
+                return "jcJ6QbSWhR_6020TE3Kr3oA8_KC0hehx1Dy9VQzi1Jw";
+            }
+        }
+
+        private static string Secret {
+            get {
+                return "iVQcyXT4Rha3LkmayAj2LbwL7VDQypMOEZHpZUu46Bk";
+            }
+        }
+    }
+}
---
+++ Unsplash.Net.Test/UnitTest1.cs
@@ -0,0 +1,35 @@
+using Unsplasharp;
+using UnsplashsharpTest.Data;
+
+namespace Unsplash.Net.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public async Task SearchPhotosTest()
+        {
+            var query = "mountains";
+            var client = new UnsplasharpClient(Credentials.ApplicationId);
+            var photosFound = await client.SearchPhotos(query);
+            var photosFoundPaged = await client.SearchPhotos(query, 2);
+
+            Assert.IsTrue(photosFound.Count > 0);
+            Assert.IsTrue(photosFoundPaged.Count > 0);
+
+            Assert.IsNotNull(client.LastPhotosSearchQuery);
+            Assert.IsTrue(client.LastPhotosSearchTotalPages > 0);
+            Assert.IsTrue(client.LastPhotosSearchTotalResults > 0);
+        }
+    }
+}
